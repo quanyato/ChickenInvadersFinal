b22c42b5025becab5033ed7b057b26be3f3015c7
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2023-10-24 19:37:20 +0700

    test

--- menu_main.py
+++ menu_main.py
@@ -19,6 +19,8 @@ def draw_background():
     for x in range (2):
         game_screen.blit(bg_image, (0, ((-x * bg_height)-bg_scroll_position)))
 
+
+
 logo_image = pygame.image.load('image/logo-small-scale.png')
 play_image = pygame.image.load('image/btn-frame.png')
 quit_image = pygame.image.load('image/btn-frame.png')
@@ -26,36 +28,55 @@ setting_image = pygame.image.load('image/btn-frame.png')
 contact_image = pygame.image.load('image/btn-frame.png')
 option_image = pygame.image.load('image/btn-frame.png')
 music_image =  pygame.image.load('image/btn-mute.png')
+music_mute_image = pygame.image.load('image/btn-menu.png')
+
+def draw_music_button(status):
+    if status:
+        game_screen.blit(music_image, [870,530])
+    else:
+        game_screen.blit(music_mute_image, [870,530])
+
+
 
 pygame.mixer.music.load('music/aov_song.wav')
 pygame.mixer.music.set_volume(0.9)
 pygame.mixer.music.play(-1)
+music_on = True
 
 play_button=button.Button(360, 284, play_image)
 setting_button=button.Button(360, 334, setting_image)
 option_button=button.Button(360, 384, option_image)
 contact_button=button.Button(360, 434, contact_image)
 quit_button=button.Button(360, 484, quit_image)
+music_button=button.Button(870,530, music_image)
 
 run=True
 while run: 
     draw_background()
+    draw_music_button(music_on)
     bg_scroll_position -= 0.1
     if abs(bg_scroll_position) > bg_height:
         bg_scroll_position=0
 
     draw_text_center('PLAY', 18, 360, 284, play_image.get_width(), play_image.get_height())
-    draw_text_center('SETTING', 18, 360, 334, play_image.get_width(), play_image.get_height())
+    draw_text_center('SETTINGS', 18, 360, 334, play_image.get_width(), play_image.get_height())
     draw_text_center('INFORMATION', 18, 360, 384, play_image.get_width(), play_image.get_height())
     draw_text_center('CONTACT US', 18, 360, 434, play_image.get_width(), play_image.get_height())
     draw_text_center('QUIT', 18, 360, 484, play_image.get_width(), play_image.get_height())
 
     game_screen.blit(logo_image, [295,60])
-    game_screen.blit(music_image, [870,530])
+    #game_screen.blit(current_image, [870,530])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
+
+    if music_button.draw()==True:
+        music_on = not music_on
+        if music_on:
+            pygame.mixer.music.play()
+        else:
+            pygame.mixer.music.pause()
 
     if quit_button.draw()==True:
         pygame.quit()

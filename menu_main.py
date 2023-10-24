7912f5555c81d5db7c64038512df019124af6565
quanyato <quanlb.b21cn607@stu.ptit.edu.vn>
2023-10-24 21:46:54 +0700

    fix music button

--- menu_main.py
+++ menu_main.py
@@ -19,24 +19,21 @@ def draw_background():
     for x in range (2):
         game_screen.blit(bg_image, (0, ((-x * bg_height)-bg_scroll_position)))
 
-
-
 logo_image = pygame.image.load('image/logo-small-scale.png')
 play_image = pygame.image.load('image/btn-frame.png')
 quit_image = pygame.image.load('image/btn-frame.png')
 setting_image = pygame.image.load('image/btn-frame.png')
 contact_image = pygame.image.load('image/btn-frame.png')
 option_image = pygame.image.load('image/btn-frame.png')
-music_image =  pygame.image.load('image/btn-unmute.png')
-music_mute_image = pygame.image.load('image/btn-mute.png')
+music_button_frame = pygame.image.load('image/btn-frame-small.png')
+music_unmute_image =  pygame.image.load('image/icon-unmute.png')
+music_mute_image = pygame.image.load('image/icon-mute.png')
 
-def draw_music_button(status):
+def draw_music_button(status=True):
     if status:
-        game_screen.blit(music_image, [870,530])
+        game_screen.blit(music_unmute_image, [870,530])
     else:
         game_screen.blit(music_mute_image, [870,530])
-
-
 
 pygame.mixer.music.load('music/aov_song.wav')
 pygame.mixer.music.set_volume(0.9)
@@ -48,12 +45,11 @@ setting_button=button.Button(360, 334, setting_image)
 option_button=button.Button(360, 384, option_image)
 contact_button=button.Button(360, 434, contact_image)
 quit_button=button.Button(360, 484, quit_image)
-music_button=button.Button(870,530, music_image)
+music_button=button.Button(870,530, music_button_frame)
 
 run=True
 while run: 
     draw_background()
-    draw_music_button(music_on)
     bg_scroll_position -= 0.1
     if abs(bg_scroll_position) > bg_height:
         bg_scroll_position=0
@@ -65,7 +61,6 @@ while run:
     draw_text_center('QUIT', 18, 360, 484, play_image.get_width(), play_image.get_height())
 
     game_screen.blit(logo_image, [295,60])
-    #game_screen.blit(current_image, [870,530])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,6 +72,7 @@ while run:
             pygame.mixer.music.play()
         else:
             pygame.mixer.music.pause()
+    draw_music_button(music_on)
 
     if quit_button.draw()==True:
         pygame.quit()

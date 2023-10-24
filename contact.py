a88d5882f0200d04f2c3224a12ea3d5f963235f0
quanyato <quanlb.b21cn607@stu.ptit.edu.vn>
2023-10-24 22:29:16 +0700

    NEW CONTACT UI

--- contact.py
+++ contact.py
@@ -2,31 +2,52 @@ import pygame
 import os
 import sys
 import button
+from display_on_screen import draw_text_center
 pygame.init()
 
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 614
-background_image = pygame.image.load('image/bg.jpg').convert()
 game_screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Chicken Invaders')
-back_image = pygame.image.load('image/back.png')
-back_image = pygame.transform.scale(back_image, (SCREEN_WIDTH//10, SCREEN_HEIGHT//10))
-imformtion_image = pygame.image.load('image/team.jpg').convert()
-imformtion_image = pygame.transform.scale(imformtion_image, (SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
 
-sound1 = pygame.mixer.Sound('music/first_date.mp3')
-music_one_time = True   # chạy nhạc 1 lần
+back_image = pygame.image.load('image/btn-back.png')
+frame_image = pygame.image.load('image/frame-setting-menu.png')
+member_frame_image = pygame.image.load('image/member-card.png')
+
+bg_image = pygame.image.load("image/back_loop.jpg").convert_alpha()
+bg_height = bg_image.get_height()
+bg_scroll_position=0
+def draw_background():
+    global bg_scroll_position
+    for x in range (2):
+        game_screen.blit(bg_image, (0, ((-x * bg_height)-bg_scroll_position)))
+
+pygame.mixer.music.load('music/aov_song.wav')
+pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.play(-1)
 
 run=True
-back_button = button.Button(30,30,back_image)
+back_button = button.Button(200,140,back_image)
 while run:
+     
+    draw_background()
+    bg_scroll_position -= 0.1
+    if abs(bg_scroll_position) > bg_height:
+        bg_scroll_position=0
+
+    game_screen.blit(frame_image, (190, 130))
+    game_screen.blit(member_frame_image, (238, 238))
+    game_screen.blit(member_frame_image, (410, 238))
+    game_screen.blit(member_frame_image, (582, 238))
+
+    draw_text_center('CONTACT US', 24, 190, 130, 580, 100)
+    draw_text_center('TRAN XUAN DAT', 14, 238, 300, 140, 30)
+    draw_text_center('D21CQCN07-B', 14, 238, 320, 140, 30)
+    draw_text_center('HA CUONG THINH', 14, 410, 300, 140, 30)
+    draw_text_center('D21CQCN07-B', 14, 410, 320, 140, 30)
+    draw_text_center('LAI BA QUAN', 14, 582, 300, 140, 30)
+    draw_text_center('D21CQCN07-B', 14, 582, 320, 140, 30)
     
-    if music_one_time:
-        sound1.play()
-        music_one_time = False
-        
-    game_screen.blit(background_image, [0, 0])
-    game_screen.blit(imformtion_image, [150, 100])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

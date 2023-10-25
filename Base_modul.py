87709d3cdde990b0dbcafa14bfdb860866aae90d
tckyato <quanlb.b21cn607@stu.ptit.edu.vn>
2023-10-25 17:15:19 +0700

    replace ship image

--- Base_modul.py
+++ Base_modul.py
@@ -9,33 +9,33 @@ pygame.init()
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 614
 BLUE = ( 0,0,255)
-font = pygame.font.SysFont('Helvetica', 20)     # font chữ
+font = pygame.font.Font('Fonts/Aldrich-Regular.ttf', 18)
 
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-base_image = pygame.image.load('image/base.png').convert_alpha()
-base_image = pygame.transform.scale(base_image, (SCREEN_WIDTH//18, SCREEN_HEIGHT//15))
+base_image = pygame.image.load('image/ship.png')
+base_width = base_image.get_rect().width   
+base_height = base_image.get_rect().height
+
 xe_no_image = pygame.image.load('image/vu_no_3.png')
 xe_no_image = pygame.transform.scale(xe_no_image, (SCREEN_WIDTH//8, SCREEN_HEIGHT//8))
-base_width = base_image.get_rect().width   
-base_height = base_image.get_rect().height 
 vu_no_image = pygame.image.load('image/vu_no.png')
 vu_no_image = pygame.transform.scale(vu_no_image, (SCREEN_WIDTH//9, SCREEN_HEIGHT//9))
 
-base_missile_image = pygame.image.load('image/dan.png').convert_alpha()
-base_missile_image  = pygame.transform.scale(base_missile_image, (30,40))
+base_missile_image = pygame.image.load('image/bullet.png')
 base_missile_width = base_missile_image.get_rect().width
 base_missile_height = base_missile_image.get_rect().height
 
-defense_image =  pygame.image.load('image/ray.png').convert_alpha()
-defense_image = pygame.transform.scale(defense_image, (SCREEN_WIDTH//6, SCREEN_HEIGHT//6))
+defense_image =  pygame.image.load('image/sheld-small.png')
+defense_width = defense_image.get_width()
+defense_height = defense_image.get_height()
 
 game_over_sound = pygame.mixer.Sound('music/game_over_music.mp3')
 winner_sound = pygame.mixer.Sound('music/winner.mp3')
 #==============================================================================================
 class Base:
 
-    x_loc = 300
+    x_loc = SCREEN_WIDTH//2 - base_width//2
     y_loc = 550
     time_boom =  0
     hp = 100
@@ -100,11 +100,13 @@ class Base_defense(Base):
     time = 200
 
     def draw_defense(self,base):
+        sheld_x = base.x_loc - defense_width//2 + base_width//2
+        sheld_y = base.y_loc - defense_height//2 + base_height//2
         if self.time > 0:
             if self.time >= 200:
-                game_screen.blit(defense_image, [base.x_loc - 55, base.y_loc - 33] )
+                game_screen.blit(defense_image, [sheld_x, sheld_y])
             else:
                 if self.time % 12 == 0:
-                    game_screen.blit(defense_image, [base.x_loc - 55, base.y_loc - 33] )
-                game_screen.blit(font.render("RUN OUT OF TIME", True, BLUE), [400,500]) # in ra thông bao sắp hết khiên
+                    game_screen.blit(defense_image, [sheld_x, sheld_y])
+                game_screen.blit(font.render("RUN OUT OF TIME", True, (255,255,255)), [400,400])
             self.time -= 1
